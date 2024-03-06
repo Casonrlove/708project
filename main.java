61dc56b419dbcbdd3d6b43390c2112c23f344af6
Casonrlove <Casonrlove@tamu.edu>
2024-03-06 15:35:00 -0600

    push

---
+++ main.java
@@ -0,0 +1,16 @@
+package project;
+
+public class main {
+    
+    //----------CALL CARD FUNCTION----------//
+
+    //----------IMPLEMENT CARDS----------//
+
+    //----------IMPLEMENT RULE1&2----------//
+
+    //----------IMPLEMENT RULE3&4----------//
+
+    //----------IMPLEMENT RULE5&6----------//
+
+    //----------IMPLEMENT RULE7&8----------//
+}
